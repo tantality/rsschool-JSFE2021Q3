f134c7b12c57de5de2793910d3af5358a64254e1
tantality <angel.tretyak@mail.ru>
2021-09-27 20:36:53 +0300

    feat: correct video section

--- museum/assets/index.js
+++ museum/assets/index.js
@@ -2,12 +2,12 @@ const progress = document.querySelector('.progress');
   
 progress.addEventListener('input', function() {
   const value = this.value;
-  this.style.background = `linear-gradient(to right, #24809E 0%, #24809E ${value}%, #C4C4C4 ${value}%, #C4C4C4 100%)`
+  this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #C4C4C4 ${value}%, #C4C4C4 100%)`
 })
  
 const progress2 = document.querySelector('.progresss');
   
 progress2.addEventListener('input', function() {
   const value = this.value;
-  this.style.background = `linear-gradient(to right, #24809E 0%, #24809E ${value}%, #C4C4C4 ${value}%, #C4C4C4 100%)`
+  this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #C4C4C4 ${value}%, #C4C4C4 100%)`
 })
