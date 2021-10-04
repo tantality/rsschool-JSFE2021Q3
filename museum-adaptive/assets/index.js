a41257cbeac40383bcf694c485d144d087db55eb
tantality <angel.tretyak@mail.ru>
2021-10-05 02:20:46 +0300

    feat: complete adaptability for a 420 pixel screen

--- museum-adaptive/assets/index.js
+++ museum-adaptive/assets/index.js
@@ -31,12 +31,14 @@ generateGallery();
 
 const menu = document.querySelector('.nav');
 const burgerIcon = document.querySelector('.burger');
-const welcomeCol=document.querySelector('.welcome__column')
+const welcomeCol=document.querySelector('.welcome__column');
+const headerCont=document.querySelector('.header__container');
 
 function toggleMenu() {
   menu.classList.toggle('open');
   burgerIcon.classList.toggle('open');
   welcomeCol.classList.toggle('opacity-none');
+  headerCont.classList.toggle('header__container-padding');
 }
 
 burgerIcon.addEventListener('click', function(e) {
@@ -53,9 +55,11 @@ document.addEventListener('click', function(e) {
 
 
 
-    mapboxgl.accessToken = 'pk.eyJ1IjoidGFudGFsaXR5IiwiYSI6ImNrdWJ1YTcyaDByemMybm12MXR3ZW55OTMifQ.AClSeJjIqoAnx5Ifygvx4w';
-    var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/light-v10'
-    });
+    // mapboxgl.accessToken = 'pk.eyJ1IjoidGFudGFsaXR5IiwiYSI6ImNrdWJ1YTcyaDByemMybm12MXR3ZW55OTMifQ.AClSeJjIqoAnx5Ifygvx4w';
+    // var map = new mapboxgl.Map({
+    // container: 'map',
+    // style: 'mapbox://styles/mapbox/light-v10'
+    // });
 
+
+    console.log('Самооценка:\n1)Вёрстка соответствует макету на экранах 1024px, 768px, 420px. На всех трех разрешениях отсутствует форма покупки билетов +108/120\n2) Ни на одном из разрешений до 320px включительно не появляется горизонтальная полоса прокрутки +6/6\n3)Совмещается адаптивная и респонсивная (резиновая) вёрстка +4/14 При изменении ширины экрана плавно изменяются размеры:\n\tкарты +2\n\tгалереи изображений и изображений в ней +2\n4)На ширине экрана 1024рх и меньше реализовано адаптивное меню +12/12\n\n Отметка: 130/160');
